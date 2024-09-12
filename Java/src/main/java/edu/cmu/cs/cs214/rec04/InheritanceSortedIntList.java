d9c41ecc3424af012c9a1e1f3dc267196fedeabd
Kiera Cawley <kieracawley@gmail.com>
2024-09-12 19:00:27 -0400

    Java inheritance sorter

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -1,5 +1,8 @@
 package edu.cmu.cs.cs214.rec04;
 
+import java.util.List;
+import java.util.ArrayList; 
+
 /**
  * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
  * count of the number of attempted element insertions (not to be confused
@@ -10,7 +13,64 @@ package edu.cmu.cs.cs214.rec04;
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList implements IntegerList{
+    // Write your implementation below with API documentation3
+    private int additions = 0;
+    private List<Integer> list = new ArrayList<>();
+
+    @Override
+    public boolean add(int num) {
+        additions += 1;
+        return this.list.add(num);
+    }
+
+    @Override
+    public boolean addAll(IntegerList list) {
+        additions += list.size();
+        for (int i = 0; i < list.size(); i++){
+            boolean addSuccessful = this.list.add(list.get(i));
+            if (!addSuccessful) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int get(int index) {
+        return this.list.get(index);
+    }
+
+    @Override
+    public boolean remove(int num) {
+        int removeInd = this.list.indexOf(num);
+        if (removeInd != -1){
+            this.list.remove(removeInd);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    @Override
+    public boolean removeAll(IntegerList list) {
+        for (int i = 0; i < list.size(); i++){
+            boolean removeSuccessful = this.remove(list.get(i));
+            if (!removeSuccessful){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    public int getTotalAdded() {
+        return additions;
+    }
 
 }
